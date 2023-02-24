82545b316fbde3905018f4d904740b7294ad18b8
Furkaan24 <91979756+Furkaan24@users.noreply.github.com>
2023-02-24 11:02:40 +0100

    keys removed

--- Assets/BallMovement.cs
+++ Assets/BallMovement.cs
@@ -15,32 +15,12 @@ public class BallMovement : MonoBehaviour
     void Start()
     {
         trail.enabled = false;
-        currentSpeed = baseSpeed;
     }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            currentSpeed = Mathf.Min(currentSpeed + acceleration * Time.deltaTime, maxSpeed);
-        }
-        else
-        {
-            currentSpeed = baseSpeed;
-        }
-
-        if (horizontalInput != 0f || verticalInput != 0f)
-        {
-            Vector3 axis = new Vector3(verticalInput, 0f, -horizontalInput).normalized;
-            transform.RotateAround(cubeTransform.position, axis, currentSpeed * Time.deltaTime);
+            Vector3 axis = new Vector3(baseSpeed, 0f, -baseSpeed).normalized;
+            transform.RotateAround(cubeTransform.position, axis, currentSpeed * Time.delgtaTime);
             trail.enabled = true;
-        }
-        else
-        {
-            trail.enabled = false;
-        }
     }
 }
